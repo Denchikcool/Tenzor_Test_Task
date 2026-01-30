5ddb26231906a46cf120500484915e80981f5c60
Denchikcool <kuzmenok.den@yandex.ru>
2026-01-30 23:36:47 +0700

    Небольшой рефакторинг кода (код-стайл)

--- pages/tensor_home_page.py
+++ pages/tensor_home_page.py
@@ -1,9 +1,13 @@
 from .base_page import BasePage
 
 class TensorHomePage(BasePage):
+    URL = "https://tensor.ru/"
     BLOCK_POWER_IN_PEOPLE = ("xpath", "//p[contains(text(),'Сила в людях')]")
     DETAILS_LINK = ("xpath", "//a[contains(@class, 'tensor_ru-link') and @href='/about']")
 
+    def is_needed_url(self):
+        return bool(self.get_url() == self.URL)
+    
     def has_power_in_people_block(self):
         return bool(self.find_element(self.BLOCK_POWER_IN_PEOPLE))
     
--- tests/test_first.py
+++ tests/test_first.py
@@ -13,7 +13,7 @@ def test_first(driver):
     contacts.switch_to_new_window()
 
     tensor_home = TensorHomePage(driver)
-    assert "https://tensor.ru/" == tensor_home.get_url()
+    assert tensor_home.is_needed_url()
     assert tensor_home.has_power_in_people_block()
     tensor_home.click_details()
 
