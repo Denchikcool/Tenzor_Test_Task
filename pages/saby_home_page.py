d3fef79fcc5d8cc189c166f760cc3b4deec8ac47
Denchikcool <kuzmenok.den@yandex.ru>
2026-02-01 16:27:20 +0700

    Переход к загрузкам

--- pages/saby_home_page.py
+++ pages/saby_home_page.py
@@ -3,7 +3,11 @@ from .base_page import BasePage
 class SabyHomePage(BasePage):
     CONTACTS_MENU = ("xpath", "//span[contains(text(),'Контакты')]")
     CONTACTS_SUB_LINK = ("xpath", "//a[contains(@class,'sbisru-link')]")
+    DOWNLOAD_LINK = ("xpath", "//a[contains(@href, 'download')]")
 
     def go_to_contacts(self):
         self.hover(self.CONTACTS_MENU)
         self.click(self.CONTACTS_SUB_LINK)
+    
+    def go_to_download(self):
+        self.click(self.DOWNLOAD_LINK)
