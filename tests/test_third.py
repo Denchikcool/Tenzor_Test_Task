ead9aee784f004da601aaaacd9a74b632e8b55a2
Denchikcool <kuzmenok.den@yandex.ru>
2026-02-01 21:08:42 +0700

    Тестирование третьего сценария

--- tests/test_third.py
+++ tests/test_third.py
@@ -4,6 +4,15 @@ from pages.saby_home_page import SabyHomePage
 from pages.saby_download_page import SabyDownloadPage
 from config import DOWNLOAD_DIR
 
+'''
+    С этим тестом два основных вопроса:
+    1. Нет в текущей версии сайта вкладки "СБИС Плагин", как указано в файле с заданием, поэтому
+    я решил скачивать с вкладки "Saby (десктоп)", т.к. в url увидел tab=plugin, а в задании
+    как раз говорилось про плагин.
+    2. На сайте не было указано в виде текста размер файла, так что пришлось читать характеристики файла (.exe)
+    по ссылке, которая хранится в теге <a>. Благодаря head и ключу Content-Length я узнал размер файла в байтах.
+'''
+
 def clear_downloads():
     shutil.rmtree(DOWNLOAD_DIR, ignore_errors = True)
     os.makedirs(DOWNLOAD_DIR, exist_ok = True)
@@ -15,13 +24,11 @@ def test_third(driver):
     saby_home.go_to_download()
 
     download_page = SabyDownloadPage(driver)
+    download_page.click_on_correct_tab()
     expected_size = download_page.get_expected_file_size()
     download_page.download_plugin()
     file_path = download_page.wait_file()
     assert os.path.exists(file_path)
     actual_size = download_page.get_real_file_size(file_path)
-
-    print("Ожидаемый размер: ", expected_size)
-    print("Фактический размер: ", actual_size)
 
     assert expected_size == actual_size
