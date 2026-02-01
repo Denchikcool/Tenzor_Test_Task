87b2fc9e8457d3b0fb37e3d82777eed80dc76ff8
Denchikcool <kuzmenok.den@yandex.ru>
2026-02-01 14:32:06 +0700

    Тестирование второго сценария

--- tests/test_second.py
+++ tests/test_second.py
@@ -1,6 +1,22 @@
-import time 
+import requests
 from pages.saby_home_page import SabyHomePage
 from pages.saby_contacts_page import SabyContactsPage
+
+'''
+    По заданию этими двумя функциями я проверяю правильно ли определяется мой текущий регион, но
+    как я выяснил, в тестах лучше не использовать сторонние регионы. Поэтому возникает вопрос:
+    нужно ли просто определять есть ли регион в поле или же создать какую-то переменную со 
+    своим регионом по типу MY_REGION = "Новосибирская обл.". Но тогда в зависимости от того, 
+    на каком компьютере это будет запускаться, нужно будет каждый раз менять регион на свой.
+    С этим моментом у меня непонятки небольшие.
+'''
+def get_my_region():
+    response = requests.get("https://ipmy.dev/api?lang=ru")
+    data = response.json()
+    return data.get("region_name")
+
+def normalize_region(region: str):
+    return region.lower().replace("область", "").replace("обл.", "").strip()
 
 def test_second(driver):
     saby_home = SabyHomePage(driver)
@@ -9,23 +25,15 @@ def test_second(driver):
 
     contacts = SabyContactsPage(driver)
     initial_region = contacts.get_region()
-    print(initial_region)
-    assert initial_region
-    print(contacts.has_partners())
+    expected_region = get_my_region()
+    assert normalize_region(expected_region) in normalize_region(initial_region)
     assert contacts.has_partners()
     initial_partners = contacts.get_partners_name()
-    #print(initial_partners)
 
     contacts.change_region()
 
-    print(contacts.get_region())
-    #assert contacts.is_region_is_kamchatka()
-    print(contacts.get_title())
-    print(contacts.get_url())
-    #assert contacts.is_title_contains_region()
-    #assert contacts.is_url_contains_region()
-    #new_partners = contacts.get_partners_name()
-    #print(new_partners)
-    #assert new_partners != initial_partners
-
-    time.sleep(15)
+    assert contacts.is_region_is_kamchatka()
+    new_partners = contacts.get_partners_name()
+    assert new_partners != initial_partners
+    assert contacts.is_title_contains_region()
+    assert contacts.is_url_contains_region()
