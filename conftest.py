a0cd28b9e5b69c22a8176e4e1d6e2e1821c9078f
Denchikcool <kuzmenok.den@yandex.ru>
2026-02-01 16:26:53 +0700

    Обновлен файл инициализации браузера расширенными настройками

--- conftest.py
+++ conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
+from config import DOWNLOAD_DIR
 
 @pytest.fixture(scope = "function")
 def driver():
-    #options = Options()
+    options = Options()
+    prefs = {
+        "download.default_directory": DOWNLOAD_DIR
+    }
+    options.add_experimental_option("prefs", prefs)
     #options.add_argument("--headless")
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options = options)
     yield driver
     driver.quit()
