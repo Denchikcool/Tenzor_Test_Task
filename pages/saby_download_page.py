17d54f6c34504d552a0396dc0d7a2483bb9b11e9
Denchikcool <kuzmenok.den@yandex.ru>
2026-02-01 22:17:33 +0700

    Добавил логи в методы

--- pages/saby_download_page.py
+++ pages/saby_download_page.py
@@ -3,6 +3,7 @@ import time
 import requests
 from .base_page import BasePage
 from config import DOWNLOAD_DIR
+from logger import logger
 
 class SabyDownloadPage(BasePage):
     PLUGIN_TAB = ("xpath", "//div[contains(@data-id,'plugin')]")
@@ -17,18 +18,26 @@ class SabyDownloadPage(BasePage):
 
     def get_expected_file_size(self):
         url = self.find_element(self.DOWNLOAD_BUTTON).get_attribute("href")
+        logger.info(f"LOGGER URL: {url}")
         req = requests.head(url, allow_redirects = True)
-        return round(int(req.headers["Content-Length"]) / 1024 / 1024, 2)
+        size = round(int(req.headers["Content-Length"]) / 1024 / 1024, 2)
+        logger.info(f"EXPECTED SIZE: {size} Mb")
+        return size
     
     def wait_file(self, timeout = 60):
         path = os.path.join(DOWNLOAD_DIR, self.FILE_NAME)
+        logger.info(f"WAIT FILE: {path}")
 
         for _ in range(timeout):
             if os.path.exists(path) and not os.path.exists(path + ".crdownload"):
+                logger.info("FILE DOWNLOADED")
                 return path
             time.sleep(1)
         
+        logger.info("DOWNLOAD TIMEOUT")
         raise TimeoutError("Файл не скачался")
     
     def get_real_file_size(self, path):
-        return round(os.path.getsize(path) / 1024 / 1024, 2)
+        size = round(os.path.getsize(path) / 1024 / 1024, 2)
+        logger.info(f"REAL SIZE: {size} Mb")
+        return size
